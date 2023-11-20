5c8eee70d26c7e334efda3f8114308b235357d96
saba-korshia <sabakorshia5@gmail.com>
2023-11-20 12:33:27 -0800

    add

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-let burger = document.querySelector(".burger_menu");
-let xMark = document.querySelector(".mark");
+let burger = document.querySelector(".burger");
+
 let mobileMenu = document.querySelector(".header_nav");
-let currencyLanguage = document.querySelector(".header_li")
+
 
 burger.addEventListener("click", () => {
     mobileMenu.classList.add("show");
-    currencyLanguage.classList.add("show_option2")
+ 
     burger.style.display = "none";
     xMark.style.display = "block";
 } )
 
 xMark.addEventListener("click", () => {
     mobileMenu.classList.remove("show");
-    currencyLanguage.classList.remove("show_option2")
+
     burger.style.display = "block";
     xMark.style.display = "none";
   }
