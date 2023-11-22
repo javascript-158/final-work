49257c8090495288f15c4db7f4909c5895eef31c
saba-korshia <sabakorshia5@gmail.com>
2023-11-22 02:34:21 -0800

    add

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-let burger = document.querySelector(".burger");
+const burger = document.querySelector(".burger");
+let xmark = document.querySelector(".mark")
+let mobileMenu = document.querySelector(".mobile_nav");
 
-let mobileMenu = document.querySelector(".header_nav");
 
-
-burger.addEventListener("click", () => {
+burger.addEventListener("click", function () {
     mobileMenu.classList.add("show");
- 
+    mobileMenu.querySelector.add(".mobile_nav")
     burger.style.display = "none";
-    xMark.style.display = "block";
+    xmark.style.display = "block";
+    mobile_nav.classList.add("show_mobile_nav")
 } )
 
-xMark.addEventListener("click", () => {
+xmark.addEventListener("click", () => {
     mobileMenu.classList.remove("show");
-
+    mobileMenu.querySelector.add(".mobile_nav")
     burger.style.display = "block";
-    xMark.style.display = "none";
+    xmark.style.display = "none";
   }
 
 );
